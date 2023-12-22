a9370a62be014a6a45f6ca214aef122a1a5ff66f
TinaTheBest <131246422+TinaTheBest@users.noreply.github.com>
2023-12-22 18:32:37 +0100

    commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="flex justify-between h-12 ">
+        <h1>DevCamp...</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
